f74a43ebdd3f010a31837be1f5bdd64bb78747a1
Trevor Anderson <tbone101@gmail.com>
2021-12-06 16:06:04 -0800

    Finished

--- src/Personcard.js
+++ src/Personcard.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Personcard = (props) => {
   const { firstName, lastName, age, hairColor } = props;
+  const [stateAge, setAge] = useState(age);
+  const handleClick = () => {
+    setAge(stateAge + 1);
+  };
   return (
     <div className="text-center m-4">
       <h1 className="font-bold text-xl">
         {firstName} {lastName}
       </h1>
-      <h2>Age: {age}</h2>
+      <h2>Age: {stateAge}</h2>
       <h2>Hair Color: {hairColor}</h2>
+      <button
+        className="border-2 border-solid border-gray-900 bg-gray-900 text-white"
+        onClick={handleClick}
+      >
+        I am Older
+      </button>
     </div>
   );
 };
